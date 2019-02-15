cca5e6701996ce3172335faa4835bf49fdac2b4d
靖守彦 <290173092@qq.com>
2019-02-15 18:23:30 +0800

    init

---
+++ src/main/java/com/github/jingshouyan/peotry/PoetryApp.java
@@ -0,0 +1,17 @@
+package com.github.jingshouyan.peotry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jingshouyan
+ * #date 2019/2/15 16:06
+ */
+
+@SpringBootApplication
+public class PoetryApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoetryApp.class, args);
+    }
+}
